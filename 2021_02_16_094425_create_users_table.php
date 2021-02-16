87252f87b9a6d392633e921e019a38827fe51d21
Wandy <wandy20number@gmail.com>
2021-02-16 18:10:26 +0700

    Penggunaan UUID
    Menggunakan UUID sebagai persiapan membuat aplikasi berskala besar

---
+++ 2021_02_16_094425_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();            
+            $table->uuid('role_id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('phone_no')->unique();
+            $table->string('password');
+            $table->timestamps();
+
+            // Foreign Key
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ 2021_02_16_103454_create_otps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otps', function (Blueprint $table) {
+            $table->uuid('id')->primary();          
+            $table->uuid('user_id');
+            $table->integer('otp_code');
+            $table->dateTime('exp_date');
+            $table->boolean('is_active');
+            $table->timestamps();
+            
+            // Foreign Key
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otps');
+    }
+}
